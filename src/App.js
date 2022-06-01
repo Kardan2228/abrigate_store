9a615a9705a5fffd0d8569817d0dde0dcc87a1f9
JChkr <49049852+Kardan2228@users.noreply.github.com>
2022-05-31 21:05:00 -0500

    The ItemListContainer.js component is created with a greeting prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
-import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
   return (
     <>
       <NavBar />
+      <ItemListContainer greeting={'Darle estilos'}/>
     </>
   );
 }
